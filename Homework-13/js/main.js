e9c44004fa6622aed4127854126d96b408ef234a
AlexSyrtsev <aleksei.turchin@gmail.com>
2021-05-26 11:49:59 +0300

    upd
    upd

--- Homework-13/js/main.js
+++ Homework-13/js/main.js
@@ -4,7 +4,8 @@ var button = document.getElementsByClassName('first-push')[0],
 button.onclick = function(event) {
 	if (JSON.parse(localStorage.getItem(arr))) {
 		display(JSON.parse(localStorage.getItem(arr)));
-	} else {
+	}
+	else {
 		ask();
 	}
 }
@@ -21,11 +22,72 @@ function ask() {
 	ajax.onload = function() {
 		var statusType = Math.round(this.status / 100);
 
+		function get(e) {
+			document.getElementsByClassName('one-guy');
+			document.getElementsByClassName('guy-info');
+			var i = 0;
+		
+			for (; i < document.getElementsByClassName('one-guy').length;) {
+				document.getElementsByClassName('one-guy')[i];
+		
+				if (e.target != document.getElementsByClassName('one-guy')[i]) {
+					document.getElementsByClassName('one-guy')[i].classList.remove('active');
+				} else {
+					document.getElementsByClassName('one-guy')[i].classList.add('active');
+					var i;
+					var j = 0;
+		
+					for (; j < document.getElementsByClassName('guy-info').length;) {
+						document.getElementsByClassName('guy-info')[j];
+		
+						if (j == i) {
+							document.getElementsByClassName('guy-info')[j].classList.remove('invisible');
+						} else {
+							document.getElementsByClassName('guy-info')[j].classList.add('invisible');
+						}
+		
+						j++;
+					}
+				}
+		
+				i++;
+			}
+		}
+		
+		function display(guys) {
+			document.getElementsByClassName('info-container');
+			document.getElementsByClassName('all-guys');
+			document.getElementsByClassName('all-guys')[0].onclick = get;
+			button.getElementsByClassName('second-push')[0].setAttribute('disabled', '');
+			localStorage.setItem(arr, JSON.stringify(guys));
+			var index = 0;
+		
+			for (; index < guys.length;) {
+				document.getElementsByClassName('all-guys')[0].innerHTML += '<div class="one-guy">User ' + (index + 1) + '</div>';
+				document.getElementsByClassName('all-guys')[0].firstElementChild.classList.add('active');
+				document.getElementsByClassName('info-container')[0].innerHTML +=
+					'<div class="guy-info invisible"><div class="guy-images"><img src="' + guys[index].avatar + '" alt="' + (index + 1) + '" /></div>' + 
+					'<div class="guy-names"><div>First Name: ' + guys[index].first_name + 
+					'<br>' + 'Last Name: ' + guys[index].last_name + 
+					'<br>' + 'E-mail: ' + guys[index].email + 
+					'<br>' + 'ID-number: ' + guys[index].id + '</div></div>';
+				document.getElementsByClassName('info-container')[0].firstElementChild.classList.remove('invisible');
+				
+				index++;
+			}
+		}
+		
+		function mistake() {
+			button.getElementsByClassName('second-push')[0].setAttribute('disabled', '');
+			document.getElementsByClassName('left-one')[0].innerHTML = '<p class="mistake">Ошибка получения данных</p>'
+		}
+
 		if (ajax.status == 200) {
 			display(JSON.parse(ajax.response).data);
 		} else {
 			mistake();
 		}
+
 		console.log((statusType === 2) ? JSON.parse(ajax.response).data : ajax.status);
 	};
 
@@ -37,63 +99,4 @@ function ask() {
 	ajax.onloadend = function() {
 		console.log('AJAX-запрос завершён.');
 	};
-}
-
-function mistake() {
-	button.getElementsByClassName('second-push')[0].setAttribute('disabled', '');
-	document.getElementsByClassName('left-one')[0].innerHTML = '<p class="mistake">Ошибка получения данных!</p>'
-}
-
-function get(e) {
-	document.getElementsByClassName('one-guy');
-	document.getElementsByClassName('guy-info');
-	var i = 0;
-
-	for (; i < document.getElementsByClassName('one-guy').length;) {
-		document.getElementsByClassName('one-guy')[i];
-		if (e.target != document.getElementsByClassName('one-guy')[i]) {
-			document.getElementsByClassName('one-guy')[i].classList.remove('active');
-		} else {
-			document.getElementsByClassName('one-guy')[i].classList.add('active');
-			var i;
-			var j = 0;
-
-			for (; j < document.getElementsByClassName('guy-info').length;) {
-				document.getElementsByClassName('guy-info')[j];
-				if (j == i) {
-					document.getElementsByClassName('guy-info')[j].classList.remove('invisible');
-				} else {
-					document.getElementsByClassName('guy-info')[j].classList.add('invisible');
-				}
-
-				j++;
-			}
-		}
-
-		i++;
-	}
-}
-
-function display(guys) {
-	document.getElementsByClassName('info-container');
-	document.getElementsByClassName('all-guys');
-	var index = 0;
-
-	for (; index < guys.length;) {
-		document.getElementsByClassName('all-guys')[0].innerHTML += '<div class="one-guy">User ' + (index + 1) + '</div>';
-		document.getElementsByClassName('all-guys')[0].firstElementChild.classList.add('active');
-		document.getElementsByClassName('info-container')[0].innerHTML +=
-			'<div class="guy-info invisible"><div class="guy-images"><img src="' + guys[index].avatar + '" alt="' + (index + 1) + '" /></div>' + 
-			'<div class="guy-names"><div>First Name: ' + guys[index].first_name + 
-			'<br>' + 'Last Name: ' + guys[index].last_name + 
-			'<br>' + 'E-mail: ' + guys[index].email + 
-			'<br>' + 'ID-number: ' + guys[index].id + '</div></div>';
-		document.getElementsByClassName('info-container')[0].firstElementChild.classList.remove('invisible');
-		
-		index++;
-	}
-
-	document.getElementsByClassName('all-guys')[0].onclick = get;
-	button.getElementsByClassName('second-push')[0].setAttribute('disabled', '');
-	localStorage.setItem(arr, JSON.stringify(guys));
 }
